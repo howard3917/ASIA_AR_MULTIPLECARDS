8fb01fd0dc3bee46ecaba4970d528407c3073cc0
howard3917 <71679856+howard3917@users.noreply.github.com>
2020-11-03 11:59:53 +0800

    rotate

--- New Unity Project/Assets/scripts/GameManager.cs
+++ New Unity Project/Assets/scripts/GameManager.cs
@@ -1,7 +1,30 @@
 ﻿using UnityEngine;
 
 public class GameManager : MonoBehaviour
-{
+{[Header("兔子")]
    public Transform rabbit;
-   public Transform bat;
+    [Header("蝙蝠")]
+    public Transform bat;
+    [Header("虛擬搖桿")]
+    public FloatingJoystick joystick;
+    [Header("旋轉速度"), Range(0.1f, 20f)]
+    public float turn = 1.5f;
+    [Header("縮放"), Range(0f, 5f)]
+    public float size = 0.3f;
+
+    private void Start()
+    {
+        print("開始事件");
+    }
+
+    private void Update()
+    {
+        print("更新事件");
+        print(joystick.Vertical);
+
+        bat.Rotate(0, joystick.Horizontal * turn , 0);
+        rabbit.Rotate(0, joystick.Horizontal * turn , 0);
+
+        bat.localScale += new Vector3(1, 1, 1) * joystick.Vertical* size;
+    }
 }
